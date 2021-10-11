cca8d508eec5b658633b1c243723534029040fb7
Cole Fortner <colefortner@gmail.com>
2021-10-10 22:06:36 -0400

    Add state logic for ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,13 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    title = "farts";
+    setTitle("Updtated");
     console.log(title);
   };
 
